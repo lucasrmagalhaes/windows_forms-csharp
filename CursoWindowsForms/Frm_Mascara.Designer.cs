4e4109b4e9a2a22370f65bb59b611eb32fb64832
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-05-31 15:31:27 -0300

    Manipulando as máscaras

--- CursoWindowsForms/Frm_Mascara.Designer.cs
+++ CursoWindowsForms/Frm_Mascara.Designer.cs
@@ -28,34 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.Msk_TextBox = new System.Windows.Forms.MaskedTextBox();
+            this.Lbl_Conteudo = new System.Windows.Forms.Label();
+            this.Lbl_MascaraAtiva = new System.Windows.Forms.Label();
+            this.Btn_Hora = new System.Windows.Forms.Button();
+            this.Btn_CEP = new System.Windows.Forms.Button();
+            this.Btn_Moeda = new System.Windows.Forms.Button();
+            this.Btn_Data = new System.Windows.Forms.Button();
+            this.Btn_Telefone = new System.Windows.Forms.Button();
+            this.Btn_VerConteudo = new System.Windows.Forms.Button();
+            this.Btn_Senha = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // Msk_TextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(432, 20);
-            this.textBox1.TabIndex = 0;
+            this.Msk_TextBox.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Msk_TextBox.Location = new System.Drawing.Point(23, 31);
+            this.Msk_TextBox.Name = "Msk_TextBox";
+            this.Msk_TextBox.Size = new System.Drawing.Size(258, 26);
+            this.Msk_TextBox.TabIndex = 0;
             // 
-            // maskedTextBox1
+            // Lbl_Conteudo
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(12, 50);
-            this.maskedTextBox1.Mask = "000,000,000-00";
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(432, 20);
-            this.maskedTextBox1.TabIndex = 1;
+            this.Lbl_Conteudo.AutoSize = true;
+            this.Lbl_Conteudo.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Conteudo.Location = new System.Drawing.Point(23, 246);
+            this.Lbl_Conteudo.Name = "Lbl_Conteudo";
+            this.Lbl_Conteudo.Size = new System.Drawing.Size(0, 19);
+            this.Lbl_Conteudo.TabIndex = 1;
+            // 
+            // Lbl_MascaraAtiva
+            // 
+            this.Lbl_MascaraAtiva.AutoSize = true;
+            this.Lbl_MascaraAtiva.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_MascaraAtiva.Location = new System.Drawing.Point(23, 75);
+            this.Lbl_MascaraAtiva.Name = "Lbl_MascaraAtiva";
+            this.Lbl_MascaraAtiva.Size = new System.Drawing.Size(0, 19);
+            this.Lbl_MascaraAtiva.TabIndex = 2;
+            // 
+            // Btn_Hora
+            // 
+            this.Btn_Hora.Location = new System.Drawing.Point(23, 110);
+            this.Btn_Hora.Name = "Btn_Hora";
+            this.Btn_Hora.Size = new System.Drawing.Size(82, 37);
+            this.Btn_Hora.TabIndex = 3;
+            this.Btn_Hora.Text = "Hora";
+            this.Btn_Hora.UseVisualStyleBackColor = true;
+            this.Btn_Hora.Click += new System.EventHandler(this.Btn_Hora_Click);
+            // 
+            // Btn_CEP
+            // 
+            this.Btn_CEP.Location = new System.Drawing.Point(111, 110);
+            this.Btn_CEP.Name = "Btn_CEP";
+            this.Btn_CEP.Size = new System.Drawing.Size(82, 37);
+            this.Btn_CEP.TabIndex = 4;
+            this.Btn_CEP.Text = "CEP";
+            this.Btn_CEP.UseVisualStyleBackColor = true;
+            this.Btn_CEP.Click += new System.EventHandler(this.Btn_CEP_Click);
+            // 
+            // Btn_Moeda
+            // 
+            this.Btn_Moeda.Location = new System.Drawing.Point(199, 110);
+            this.Btn_Moeda.Name = "Btn_Moeda";
+            this.Btn_Moeda.Size = new System.Drawing.Size(82, 37);
+            this.Btn_Moeda.TabIndex = 5;
+            this.Btn_Moeda.Text = "Moeda";
+            this.Btn_Moeda.UseVisualStyleBackColor = true;
+            this.Btn_Moeda.Click += new System.EventHandler(this.Btn_Moeda_Click);
+            // 
+            // Btn_Data
+            // 
+            this.Btn_Data.Location = new System.Drawing.Point(23, 153);
+            this.Btn_Data.Name = "Btn_Data";
+            this.Btn_Data.Size = new System.Drawing.Size(82, 37);
+            this.Btn_Data.TabIndex = 6;
+            this.Btn_Data.Text = "Data";
+            this.Btn_Data.UseVisualStyleBackColor = true;
+            this.Btn_Data.Click += new System.EventHandler(this.Btn_Data_Click);
+            // 
+            // Btn_Telefone
+            // 
+            this.Btn_Telefone.Location = new System.Drawing.Point(199, 153);
+            this.Btn_Telefone.Name = "Btn_Telefone";
+            this.Btn_Telefone.Size = new System.Drawing.Size(82, 37);
+            this.Btn_Telefone.TabIndex = 7;
+            this.Btn_Telefone.Text = "Telefone";
+            this.Btn_Telefone.UseVisualStyleBackColor = true;
+            this.Btn_Telefone.Click += new System.EventHandler(this.Btn_Telefone_Click);
+            // 
+            // Btn_VerConteudo
+            // 
+            this.Btn_VerConteudo.Location = new System.Drawing.Point(23, 196);
+            this.Btn_VerConteudo.Name = "Btn_VerConteudo";
+            this.Btn_VerConteudo.Size = new System.Drawing.Size(258, 37);
+            this.Btn_VerConteudo.TabIndex = 8;
+            this.Btn_VerConteudo.Text = "Ver Conteúdo";
+            this.Btn_VerConteudo.UseVisualStyleBackColor = true;
+            this.Btn_VerConteudo.Click += new System.EventHandler(this.Btn_VerConteudo_Click);
+            // 
+            // Btn_Senha
+            // 
+            this.Btn_Senha.Location = new System.Drawing.Point(111, 153);
+            this.Btn_Senha.Name = "Btn_Senha";
+            this.Btn_Senha.Size = new System.Drawing.Size(82, 37);
+            this.Btn_Senha.TabIndex = 9;
+            this.Btn_Senha.Text = "Senha";
+            this.Btn_Senha.UseVisualStyleBackColor = true;
+            this.Btn_Senha.Click += new System.EventHandler(this.Btn_Senha_Click);
             // 
             // Frm_Mascara
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.maskedTextBox1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(312, 283);
+            this.Controls.Add(this.Btn_Senha);
+            this.Controls.Add(this.Btn_VerConteudo);
+            this.Controls.Add(this.Btn_Telefone);
+            this.Controls.Add(this.Btn_Data);
+            this.Controls.Add(this.Btn_Moeda);
+            this.Controls.Add(this.Btn_CEP);
+            this.Controls.Add(this.Btn_Hora);
+            this.Controls.Add(this.Lbl_MascaraAtiva);
+            this.Controls.Add(this.Lbl_Conteudo);
+            this.Controls.Add(this.Msk_TextBox);
             this.Name = "Frm_Mascara";
-            this.Text = "Frm_Mascara";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Exemplos de Máscaras";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -63,7 +161,15 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox Msk_TextBox;
+        private System.Windows.Forms.Label Lbl_Conteudo;
+        private System.Windows.Forms.Label Lbl_MascaraAtiva;
+        private System.Windows.Forms.Button Btn_Hora;
+        private System.Windows.Forms.Button Btn_CEP;
+        private System.Windows.Forms.Button Btn_Moeda;
+        private System.Windows.Forms.Button Btn_Data;
+        private System.Windows.Forms.Button Btn_Telefone;
+        private System.Windows.Forms.Button Btn_VerConteudo;
+        private System.Windows.Forms.Button Btn_Senha;
     }
 }
--- CursoWindowsForms/Frm_Mascara.cs
+++ CursoWindowsForms/Frm_Mascara.cs
@@ -16,5 +16,70 @@ namespace CursoWindowsForms
         {
             InitializeComponent();
         }
+
+        private void Btn_Hora_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Mask = "00:00";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_VerConteudo_Click(object sender, EventArgs e)
+        {
+            Lbl_Conteudo.Text = Msk_TextBox.Text;
+        }
+
+        private void Btn_CEP_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Mask = "00000-000";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_Moeda_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Mask = "$ 000,000,000.00";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_Data_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Mask = "00/00/0000";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_Telefone_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = false;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Mask = "(00) 00000-0000";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
+
+        private void Btn_Senha_Click(object sender, EventArgs e)
+        {
+            Msk_TextBox.UseSystemPasswordChar = true;
+            Lbl_Conteudo.Text = "";
+            Msk_TextBox.Mask = "000000";
+            Lbl_MascaraAtiva.Text = Msk_TextBox.Mask;
+            Msk_TextBox.Text = "";
+            Msk_TextBox.Focus();
+        }
     }
 }
