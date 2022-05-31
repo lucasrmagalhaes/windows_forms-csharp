14066bd1c7b3cb6b059f7a8dfcf79658008f0771
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-05-31 11:50:59 -0300

    Botão e eventos

--- CursoWindowsForms/Frm_HelloWorld.Designer.cs
+++ CursoWindowsForms/Frm_HelloWorld.Designer.cs
@@ -29,25 +29,37 @@
         private void InitializeComponent()
         {
             this.lbl_Titulo = new System.Windows.Forms.Label();
+            this.Btn_Sair = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lbl_Titulo
             // 
             this.lbl_Titulo.AutoSize = true;
             this.lbl_Titulo.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Titulo.Location = new System.Drawing.Point(30, 30);
+            this.lbl_Titulo.Location = new System.Drawing.Point(12, 34);
             this.lbl_Titulo.Name = "lbl_Titulo";
             this.lbl_Titulo.Size = new System.Drawing.Size(213, 19);
             this.lbl_Titulo.TabIndex = 0;
             this.lbl_Titulo.Text = "Visual Studio .NET Version";
             this.lbl_Titulo.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
+            // Btn_Sair
+            // 
+            this.Btn_Sair.Location = new System.Drawing.Point(402, 325);
+            this.Btn_Sair.Name = "Btn_Sair";
+            this.Btn_Sair.Size = new System.Drawing.Size(150, 30);
+            this.Btn_Sair.TabIndex = 1;
+            this.Btn_Sair.Text = "Fechar a aplicação";
+            this.Btn_Sair.UseVisualStyleBackColor = true;
+            this.Btn_Sair.Click += new System.EventHandler(this.Btn_Sair_Click);
+            // 
             // Frm_HelloWorld
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(468, 367);
+            this.ClientSize = new System.Drawing.Size(564, 367);
+            this.Controls.Add(this.Btn_Sair);
             this.Controls.Add(this.lbl_Titulo);
             this.Name = "Frm_HelloWorld";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -60,6 +72,7 @@
         #endregion
 
         private System.Windows.Forms.Label lbl_Titulo;
+        private System.Windows.Forms.Button Btn_Sair;
     }
 }
 
--- CursoWindowsForms/Frm_HelloWorld.cs
+++ CursoWindowsForms/Frm_HelloWorld.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CursoWindowsForms
@@ -15,6 +8,11 @@ namespace CursoWindowsForms
         public Frm_HelloWorld()
         {
             InitializeComponent();
+        }
+
+        private void Btn_Sair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
