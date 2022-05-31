30c348c90b2d3941b6cc5e9c487470615063d92f
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-05-31 16:01:37 -0300

    Criando uma classe de funções

--- CursoWindowsForms/Frm_ValidaCPF.cs
+++ CursoWindowsForms/Frm_ValidaCPF.cs
@@ -11,56 +11,6 @@ namespace CursoWindowsForms
             InitializeComponent();
         }
 
-        public static bool Valida(string cpf)
-        {
-            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
-
-            string tempCpf;
-            string digito;
-            
-            int soma;
-            int resto;
-            
-            cpf = cpf.Trim();
-            cpf = cpf.Replace(".", "").Replace("-", "");
-            
-            if (cpf.Length != 11)
-                return false;
-
-            tempCpf = cpf.Substring(0, 9);
-            soma = 0;
-
-            for (int i = 0; i < 9; i++)
-                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
-
-            resto = soma % 11;
-            
-            if (resto < 2)
-                resto = 0;
-            else
-                resto = 11 - resto;
-            
-            digito = resto.ToString();
-            
-            tempCpf = tempCpf + digito;
-            soma = 0;
-            
-            for (int i = 0; i < 10; i++)
-                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
-            
-            resto = soma % 11;
-            
-            if (resto < 2)
-                resto = 0;
-            else
-                resto = 11 - resto;
-            
-            digito = digito + resto.ToString();
-            
-            return cpf.EndsWith(digito);
-        }
-
         private void Btn_Reset_Click(object sender, EventArgs e)
         {
             Lbl_Resultado.Text = "";
@@ -71,7 +21,7 @@ namespace CursoWindowsForms
         {
             bool validaCPF = false;
 
-            validaCPF = Valida(Msk_CPF.Text);
+            validaCPF = Cls_Uteis.ValidaCPF(Msk_CPF.Text);
 
             if (validaCPF == true)
             {
