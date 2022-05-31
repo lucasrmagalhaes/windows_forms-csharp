24899a48e4e34d0bd6805acee4b6bff4335bc053
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-05-31 14:43:02 -0300

    Exemplo prático

---
+++ CursoWindowsForms/Frm_ValidaSenha.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace CursoWindowsForms
+{
+    partial class Frm_ValidaSenha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txt_Senha = new System.Windows.Forms.TextBox();
+            this.Lbl_Resultado = new System.Windows.Forms.Label();
+            this.Btn_Reset = new System.Windows.Forms.Button();
+            this.Btn_Valida = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Txt_Senha
+            // 
+            this.Txt_Senha.Location = new System.Drawing.Point(12, 45);
+            this.Txt_Senha.Name = "Txt_Senha";
+            this.Txt_Senha.Size = new System.Drawing.Size(218, 20);
+            this.Txt_Senha.TabIndex = 0;
+            this.Txt_Senha.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Txt_Senha_KeyDown);
+            // 
+            // Lbl_Resultado
+            // 
+            this.Lbl_Resultado.AutoSize = true;
+            this.Lbl_Resultado.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Resultado.Location = new System.Drawing.Point(12, 90);
+            this.Lbl_Resultado.Name = "Lbl_Resultado";
+            this.Lbl_Resultado.Size = new System.Drawing.Size(0, 22);
+            this.Lbl_Resultado.TabIndex = 1;
+            // 
+            // Btn_Reset
+            // 
+            this.Btn_Reset.Location = new System.Drawing.Point(245, 42);
+            this.Btn_Reset.Name = "Btn_Reset";
+            this.Btn_Reset.Size = new System.Drawing.Size(111, 23);
+            this.Btn_Reset.TabIndex = 2;
+            this.Btn_Reset.Text = "Limpa";
+            this.Btn_Reset.UseVisualStyleBackColor = true;
+            this.Btn_Reset.Click += new System.EventHandler(this.Btn_Reset_Click);
+            // 
+            // Btn_Valida
+            // 
+            this.Btn_Valida.Location = new System.Drawing.Point(245, 71);
+            this.Btn_Valida.Name = "Btn_Valida";
+            this.Btn_Valida.Size = new System.Drawing.Size(111, 23);
+            this.Btn_Valida.TabIndex = 3;
+            this.Btn_Valida.Text = "Valida";
+            this.Btn_Valida.UseVisualStyleBackColor = true;
+            // 
+            // Frm_ValidaSenha
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 154);
+            this.Controls.Add(this.Btn_Valida);
+            this.Controls.Add(this.Btn_Reset);
+            this.Controls.Add(this.Lbl_Resultado);
+            this.Controls.Add(this.Txt_Senha);
+            this.Name = "Frm_ValidaSenha";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Validação da Senha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt_Senha;
+        private System.Windows.Forms.Label Lbl_Resultado;
+        private System.Windows.Forms.Button Btn_Reset;
+        private System.Windows.Forms.Button Btn_Valida;
+    }
+}
