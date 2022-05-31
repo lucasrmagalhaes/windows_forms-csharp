c1fddb680126b40dac3e75f1dcf239571892a6c7
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-05-31 13:54:43 -0300

    Construindo um novo formulário

---
+++ CursoWindowsForms/Frm_DemonstracaoKey.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace CursoWindowsForms
+{
+    partial class Frm_DemonstracaoKey
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txt_Input = new System.Windows.Forms.TextBox();
+            this.Txt_Msg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Txt_Input
+            // 
+            this.Txt_Input.Location = new System.Drawing.Point(8, 8);
+            this.Txt_Input.Name = "Txt_Input";
+            this.Txt_Input.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Input.TabIndex = 0;
+            // 
+            // Txt_Msg
+            // 
+            this.Txt_Msg.Location = new System.Drawing.Point(8, 40);
+            this.Txt_Msg.Multiline = true;
+            this.Txt_Msg.Name = "Txt_Msg";
+            this.Txt_Msg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Txt_Msg.Size = new System.Drawing.Size(304, 232);
+            this.Txt_Msg.TabIndex = 1;
+            this.Txt_Msg.TabStop = false;
+            // 
+            // Frm_DemonstracaoKey
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 281);
+            this.Controls.Add(this.Txt_Msg);
+            this.Controls.Add(this.Txt_Input);
+            this.Name = "Frm_DemonstracaoKey";
+            this.Text = "Demonstração Evento Key";
+            this.Load += new System.EventHandler(this.Frm_DemonstracaoKey_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt_Input;
+        private System.Windows.Forms.TextBox Txt_Msg;
+    }
+}
---
+++ CursoWindowsForms/Frm_DemonstracaoKey.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_DemonstracaoKey : Form
+    {
+        public Frm_DemonstracaoKey()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_DemonstracaoKey_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
