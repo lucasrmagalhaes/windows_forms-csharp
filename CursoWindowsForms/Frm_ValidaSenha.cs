fd24e72dcf6b93c1cfe8bb81ccb775615391b200
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-06-01 21:18:56 -0300

    Passando outra função para a biblioteca

--- CursoWindowsForms/Frm_ValidaSenha.cs
+++ CursoWindowsForms/Frm_ValidaSenha.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Drawing;
-using System.Text.RegularExpressions;
 using System.Windows.Forms;
+using CursoWindowsFormsBiblioteca;
 
 namespace CursoWindowsForms
 {
@@ -14,97 +14,6 @@ namespace CursoWindowsForms
             InitializeComponent();
         }
 
-        public class ChecaForcaSenha
-        {
-            public enum ForcaDaSenha
-            {
-                Inaceitavel,
-                Fraca,
-                Aceitavel,
-                Forte,
-                Segura
-            }
-
-            public int geraPontosSenha(string senha)
-            {
-                if (senha == null) return 0;
-
-                int pontosPorTamanho = GetPontoPorTamanho(senha);
-                int pontosPorMinusculas = GetPontoPorMinusculas(senha);
-                int pontosPorMaiusculas = GetPontoPorMaiusculas(senha);
-                int pontosPorDigitos = GetPontoPorDigitos(senha);
-                int pontosPorSimbolos = GetPontoPorSimbolos(senha);
-                int pontosPorRepeticao = GetPontoPorRepeticao(senha);
-                
-                return pontosPorTamanho + pontosPorMinusculas +
-                    pontosPorMaiusculas + pontosPorDigitos +
-                    pontosPorSimbolos - pontosPorRepeticao;
-            }
-
-            private int GetPontoPorTamanho(string senha)
-            {
-                return Math.Min(10, senha.Length) * 7;
-            }
-
-            private int GetPontoPorMinusculas(string senha)
-            {
-                int rawplacar = senha.Length - Regex.Replace(senha, "[a-z]", "").Length;
-
-                return Math.Min(2, rawplacar) * 5;
-            }
-
-            private int GetPontoPorMaiusculas(string senha)
-            {
-                int rawplacar = senha.Length - Regex.Replace(senha, "[A-Z]", "").Length;
-
-                return Math.Min(2, rawplacar) * 5;
-            }
-
-            private int GetPontoPorDigitos(string senha)
-            {
-                int rawplacar = senha.Length - Regex.Replace(senha, "[0-9]", "").Length;
-
-                return Math.Min(2, rawplacar) * 6;
-            }
-
-            private int GetPontoPorSimbolos(string senha)
-            {
-                int rawplacar = Regex.Replace(senha, "[a-zA-Z0-9]", "").Length;
-
-                return Math.Min(2, rawplacar) * 5;
-            }
-
-            private int GetPontoPorRepeticao(string senha)
-            {
-                System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex(@"(\w)*.*\1");
-                bool repete = regex.IsMatch(senha);
-                if (repete)
-                {
-                    return 30;
-                }
-                else
-                {
-                    return 0;
-                }
-            }
-
-            public ForcaDaSenha GetForcaDaSenha(string senha)
-            {
-                int placar = geraPontosSenha(senha);
-
-                if (placar < 50)
-                    return ForcaDaSenha.Inaceitavel;
-                else if (placar < 60)
-                    return ForcaDaSenha.Fraca;
-                else if (placar < 80)
-                    return ForcaDaSenha.Aceitavel;
-                else if (placar < 100)
-                    return ForcaDaSenha.Forte;
-                else
-                    return ForcaDaSenha.Segura;
-            }
-        }
-
         private void Btn_Reset_Click(object sender, EventArgs e)
         {
             Txt_Senha.Text = "";
@@ -113,8 +22,8 @@ namespace CursoWindowsForms
 
         private void Txt_Senha_KeyDown(object sender, KeyEventArgs e)
         {
-            ChecaForcaSenha verifica = new ChecaForcaSenha();
-            ChecaForcaSenha.ForcaDaSenha forca;
+            Cls_Uteis.ChecaForcaSenha verifica = new Cls_Uteis.ChecaForcaSenha();
+            Cls_Uteis.ChecaForcaSenha.ForcaDaSenha forca;
 
             forca = verifica.GetForcaDaSenha(Txt_Senha.Text);
 
