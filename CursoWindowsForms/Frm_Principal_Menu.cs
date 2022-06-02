e992f7136a781dfb86941ae98949f89cc21f2014
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-06-02 08:56:05 -0300

    Utilizando menus

---
+++ CursoWindowsForms/Frm_Principal_Menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_Principal_Menu : Form
+    {
+        public Frm_Principal_Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void novoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
