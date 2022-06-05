617878f50b42b1a3dc8ae9e10cdcad9ad28bac1d
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-06-05 14:39:23 -0300

    Programando as ações do menu

--- CursoWindowsForms/Frm_Menu_Flutuante.cs
+++ CursoWindowsForms/Frm_Menu_Flutuante.cs
@@ -29,18 +29,36 @@ namespace CursoWindowsForms
 
                 var ContextMenu = new ContextMenuStrip();
 
-                ContextMenu.Items.Add(DesenhaItemMenu("Item do Menu 01", "key"));
-                ContextMenu.Items.Add(DesenhaItemMenu("Item do Menu 02", "Frm_ValidaSenha"));
+                var vToolTip001 = DesenhaItemMenu("Item do Menu 01", "key");
+                var vToolTip002 = DesenhaItemMenu("Item do Menu 02", "Frm_ValidaSenha");
+
+                ContextMenu.Items.Add(vToolTip001);
+                ContextMenu.Items.Add(vToolTip002);
 
                 ContextMenu.Show(this, new System.Drawing.Point(e.X, e.Y));
+
+                vToolTip001.Click += new System.EventHandler(vToolTip001_Click);
+                vToolTip002.Click += new System.EventHandler(vToolTip002_Click);
+            }
+
+            void vToolTip001_Click(object sender1, EventArgs e1)
+            {
+                MessageBox.Show("Selecionei a opção do menu 001.");
+            }
+
+            void vToolTip002_Click(object sender2, EventArgs e2)
+            {
+                MessageBox.Show("Selecionei a opção do menu 002.");
             }
 
             ToolStripMenuItem DesenhaItemMenu(string texto, string nomeImagem)
             {
                 var vToolTip = new ToolStripMenuItem();
+
                 vToolTip.Text = texto;
 
                 Image MinhaImagem = (Image)global::CursoWindowsForms.Properties.Resources.ResourceManager.GetObject(nomeImagem);
+                
                 vToolTip.Image = MinhaImagem;
 
                 return vToolTip;
